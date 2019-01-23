01c1fe8f97a7e6a0354bd38b481ff6ed7209a8a1
KolbyMcGarrah <Kolbymcgarrah@gmail.com>
2019-01-22 17:00:45 -0700

    Initializing project

---
+++ routes/comments.js
@@ -0,0 +1,14 @@
+module.exports = {
+    getComments(req,res){
+        
+    },
+    addComments(req,res){
+
+    },
+    updateComments(req,res){
+
+    },
+    removeComments(req,res){
+
+    }
+}
